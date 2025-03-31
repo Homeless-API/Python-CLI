9bba872815b795916de5db929821e3ee7cf974fa
Repeat-B1nary <repeatbinary0@gmail.com>
2025-03-31 16:24:41 -0400

    GitHub_User_Activity version 1.0 compelete

--- GitHub_User_Activity/app.py
+++ GitHub_User_Activity/app.py
@@ -29,17 +29,40 @@ def write_data(response):
 
 write_data(response)
 
-'''def read_data():
-    data_path = os.path.join(parent_path, 'data')
-    with open('data.json', 'r') as f:
+def read_data():
+    data_path = os.path.join(parent_path, 'data.json')
+    with open(data_path, 'r') as f:
         data = json.load(f)
-        return data'''
+        return data
     
-
+data = read_data()
+    
+print("\n")
 print("What would you like to fetch: ")
+print("*"*30)
 print("1. Commits")
 print("2. Pull requests")
 print("3. Issues")
 print("4. All")
+print("5. Exit")
+print("*"*30)
+print("\n")
 task_picked = input("pick a number with the corresponding task: ")
 
+if task_picked == "1":
+    for type in data:
+        if type['type'] == 'PushEvent':
+            print(type['payload']['commits'])
+elif task_picked == "2":
+    for type in data:
+        if type['type'] == 'PullRequestEvent':
+            print(type['payload']['pull_request'])
+elif task_picked == "3":
+    for type in data:
+        if type['type'] == 'IssuesEvent':
+            print(type['payload']['issue'])
+elif task_picked == "4":
+    for type in data:
+        print(type['type'])
+elif task_picked == "5":
+    quit
